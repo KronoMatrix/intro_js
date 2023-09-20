856714409898c18f9f070852482a53425f1d0cff
KronoMatrix <erick-rubi@hotmail.com>
2023-09-19 22:00:49 -0600

    feat: metodos de arreglos

---
+++ intro-js-web-29c/4.1-arreglos de Erick/app.js
@@ -0,0 +1,95 @@
+//const ageP = Number(prompt('Cuantos años tienes?'));
+/*
+const age = 19;
+const retAge = 65;
+
+function calcRet() {
+  console.log(retAge - age);
+}
+
+calcRet();
+//const retAge = 65;
+
+const calcRet = function (age){
+  return retAge - age;
+}
+
+const yaxRet = calcRet(29);
+const ivanRet = calcRet(27);
+const josueRet = calcRet(25);
+
+console.log({yaxRet});
+console.log({ivanRet});
+console.log({josueRet});
+*/
+/*
+const retAge = 65;
+const year = 2023;
+
+const calcAge = function (birthYear) {
+  return year - birthYear;
+}
+
+const yaxAge = calcAge(1993);
+console.log(yaxAge)
+
+const calcRet = function (birthYear){
+  const userAge = calcAge(birthYear);
+  return retAge - userAge;
+}
+
+const yaxRet = calcRet(1993);
+console.log(yaxRet);
+*/
+
+/*function suma(a, b) {
+    console.log(`${a} + ${b} = ${a + b}`);
+  }
+  
+  function resta(a, b) {
+    console.log(`${a} - ${b} = ${a - b}`);
+  }
+  
+  function multiplicacion(a, b) {
+    console.log(`${a} x ${b} = ${a * b}`);
+  }
+  
+  function calculadora (operador, a, b) {
+    switch (operador) {
+      case '+':
+        suma(a, b);
+        break;
+      case '-':
+        resta(a, b);
+        break;
+      case '*':
+        multiplicacion(a, b);
+        break;
+    }
+  }
+  
+  calculadora('+', 1, 3);
+  calculadora('-', 1, 3);
+  calculadora('*', 1, 3);
+  */
+
+  const frutas = ['manzana', 'platano', 'uvas', 'fresas' ];
+
+  const verduras = ['cebolla', 'jitomate', 'tomate' ];
+
+  console.log(`mi fruta favorita es: ${frutas[2]}`)
+
+ // frutas[0] = 'kiwi';
+  console.log(frutas);
+  /*console.log(frutas.length);
+  console.log(verduras.length);
+
+  console.log(verduras.includes('cebolla'));
+  console.log(verduras.includes('limon'));
+  console.log(verduras.indexOf('cebolla'));
+  console.log(verduras.indexOf('limon'));
+
+  const verdurasStr = 'cebolla,jitomate,tomate';
+  const verdurasArr = verdurasStr.split(',');
+  console.log(verdurasArr);
+*/
